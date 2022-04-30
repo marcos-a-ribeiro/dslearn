64d032b5351878abfb4e2e5a711226f7ac95587d
Marcos Antonio Ribeiro <maribeiro9600@gmail.com>
2022-04-30 12:58:22 -0300

    Offer

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -16,3 +16,5 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
 INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'https://w7.pngwing.com/pngs/797/100/png-transparent-course-training-class-professional-certification-education-courses-miscellaneous-angle-business-thumbnail.png', 'https://p.kindpng.com/picc/s/191-1918594_switch-course-grey-arrows-clip-art-png-re.png');
 
+INSERT INTO tb_offer(edition, start_moment, end_moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-11-20T3:00Z', TIMESTAMP WITH TIME ZONE '2021-11-20T3:00Z', 1);
+INSERT INTO tb_offer(edition, start_moment, end_moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2020-12-20T3:00Z', TIMESTAMP WITH TIME ZONE '2021-12-20T3:00Z', 1);
